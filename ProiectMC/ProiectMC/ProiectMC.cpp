b6d229d9865ceef694369a7a826103936f49a048
Aleee2601 <alexandraonose@yahoo.com>
2024-11-01 12:01:11 +0200

    Create project

---
+++ ProiectMC/ProiectMC/ProiectMC.cpp
@@ -0,0 +1,9 @@
+// ProiectMC.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
