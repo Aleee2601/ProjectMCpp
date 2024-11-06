1bce5db034e822db84dfb973e20f2975ff78fdfe
gabrielapetcu <gabriela.petcu@student.unitbv.ro>
2024-11-06 15:39:43 +0200

    Refactoring Bullet Class

--- ProiectMC/ProiectMC/Bullet.cpp
+++ ProiectMC/ProiectMC/Bullet.cpp
@@ -1,1 +1,46 @@
-#include "bullet.h"
+#pragma once
+#include<iostream>
+#include<unordered_map>
+
+enum class Direction { UP, DOWN, LEFT, RIGHT };
+
+class Bullet
+{
+private:
+	int x, y;
+	Direction direction;
+	int endX, endY;
+
+public:
+	Bullet(int startX, int startY, Direction dir) :x(startX), y(startY), direction(dir) {}
+
+	//adaug constructor care ma va ajuta sa verific pozitia finala a glontului
+	Bullet(int startX, int startY, int endX, int endY, Direction dir)
+		: x(startX), y(startY), endX(endX), endY(endY), direction(dir) {}
+
+	//se utilizeaza o harta pentru a stoca offset-urile asociate fiecarei directii posibile
+	void move() {
+		static const std::unordered_map<Direction, std::pair<int, int>> directionOffsets = {
+			{Direction::UP, {0, -1}},
+			{Direction::DOWN, {0, 1}},
+			{Direction::LEFT, {-1, 0}},
+			{Direction::RIGHT, {1, 0}}
+		};
+		x += directionOffsets.at(direction).first;
+		y += directionOffsets.at(direction).second;
+	}
+
+	//verifica daca glontul a iesit din limitele ariei de joc
+	bool isOutOfBounds(int width, int height) const {
+		return (x < 0 || x >= width || y < 0 || y >= height);
+	}
+
+	//afiseaza un mesaj in consola daca glontul a iesit din limitele ariei
+	void logOutOfBounds(int width, int height) const {
+		if (isOutOfBounds(width, height)) {
+			std::cout << "Bullet out of bounds at position: (" << x << "," << y << ")\n";
+		}
+	}
+
+};
+
--- ProiectMC/ProiectMC/Bullet.h
+++ ProiectMC/ProiectMC/Bullet.h
@@ -1,30 +1,28 @@
 #pragma once
-#include<iostream>
+#include <unordered_map>
+#include <iostream>
+
 enum class Direction { UP, DOWN, LEFT, RIGHT };
+
 class Bullet
 {
 private:
-	int x, y;
-	Direction direction;
-public:
-	Bullet(int startX, int startY, Direction dir) :x(startX), y(startY), direction(dir){}
-	void move() {
-		switch (direction) {
-		case Direction::UP: --y; break;
-		case Direction::DOWN: ++y; break;
-		case Direction::LEFT: --x; break;
-		case Direction::RIGHT: ++x; break;
-		}
-	}
-	bool isOutOfBounds(int width, int height) const {
-		if (x < 0 || x >= width || y < 0 || y >= height)
-		{
-			std::cout << "Bullet out of bounds at position: ("
-				<< x << "," << y << ")\n";
-			return true;
-		}
+    int x, y;                 
+    Direction direction;      
+    int endX, endY;           
 
-		return false;
-	}
+public:
+    
+    Bullet(int startX, int startY, Direction dir);
+    Bullet(int startX, int startY, int endX, int endY, Direction dir);
+
+    
+    void move();
+
+    
+    bool isOutOfBounds(int width, int height) const;
+
+    
+    void logOutOfBounds(int width, int height) const;
 };
 
