a7bf8b6df2137c8e97ac453bd024510998b8d193
milicaandreea <andreea.milica@student.unitbv.ro>
2024-11-27 23:01:19 +0200

    Add addPlayer() in Client.cpp

--- ProiectMC/Client/Client.cpp
+++ ProiectMC/Client/Client.cpp
@@ -27,3 +27,32 @@ void listPlayers() {
         std::cerr << "Error fetching players. Status code: " << response.status_code << '\n';
     }
 }
+void addPlayer() {
+    int id, x, y;
+    std::string name;
+
+    std::cout << "Enter player ID: ";
+    std::cin >> id;
+    std::cout << "Enter player name: ";
+    std::cin >> name;
+    std::cout << "Enter X position: ";
+    std::cin >> x;
+    std::cout << "Enter Y position: ";
+    std::cin >> y;
+
+    auto response = cpr::Post(
+        cpr::Url{ "http://localhost:8080/add_player" },
+        cpr::Body{ "{\"id\":" + std::to_string(id) +
+                  ",\"name\":\"" + name +
+                  "\",\"x\":" + std::to_string(x) +
+                  ",\"y\":" + std::to_string(y) + "}" },
+        cpr::Header{ {"Content-Type", "application/json"} }
+    );
+
+    if (response.status_code == 200) {
+        std::cout << "Player added successfully!\n";
+    }
+    else {
+        std::cerr << "Error adding player. Status code: " << response.status_code << '\n';
+    }
+}
