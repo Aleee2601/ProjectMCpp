64187f61b39f627a57f58f108d4485d091d6b324
milicaandreea <andreea.milica@student.unitbv.ro>
2024-12-08 22:32:18 +0200

    Add bomb rendering to MainWindow class

--- ProiectMC/Client/include/MainWindow.h
+++ ProiectMC/Client/include/MainWindow.h
@@ -1,16 +1,27 @@
 #pragma once
 
 #include <QMainWindow>
-#include "ui_MainWindow.h"
+#include <QGraphicsScene>
+#include <QGraphicsEllipseItem>
+#include "../include/Bomb.h"
 
-class MainWindow : public QMainWindow
-{
-	Q_OBJECT
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow {
+    Q_OBJECT
 
 public:
-	MainWindow(QWidget *parent = nullptr);
-	~MainWindow();
+    MainWindow(QWidget* parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void onDetonateBomb(); // Slot pentru butonul de detonare
 
 private:
-	Ui::MainWindowClass ui;
+    Ui::MainWindow* ui;
+    QGraphicsScene* scene;  // Scena pentru desenare
+    QGraphicsEllipseItem* bombItem;  // Reprezentarea grafica a bombei
+    Bomb* bomb;  // Obiectul bomba
 };
