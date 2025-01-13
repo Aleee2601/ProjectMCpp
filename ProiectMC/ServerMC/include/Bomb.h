7691b3d45edf4ac9e38350f1b2fe5e05a90af218
gabrielapetcu <gabriela.petcu@student.unitbv.ro>
2025-01-13 19:15:04 +0200

    Refactor Bomb class

--- ProiectMC/ServerMC/include/Bomb.h
+++ ProiectMC/ServerMC/include/Bomb.h
@@ -1,27 +1,17 @@
 ﻿#pragma once
-#include <vector>
-#include <functional> // Pentru std::function
 
-class Bomb
-{
-public:
-    // Constructor: Initializes the bomb's position.
-    Bomb(int x, int y);
 
-    // Detonates the bomb using a callback to destroy walls
-    void Detonate(std::function<void(int, int)> destroyWallCallback);
-   
-    bool IsInactive();
-    // Calculates explosion effects without direct references to Map or Player
-    void CalculateExplosionEffects(
-        std::function<bool(int, int)> isWallDestructibleCallback,
-        std::function<void(int, int)> destroyWallCallback,
-        std::function<void(int, int)> applyDamageToPlayerCallback
-    );
+class Bomb {
 private:
-    // Bomb's position on the x-axis.
-    int m_x;
+    int posX, posY; // Poziția bombei
+    bool isActive;  // Dacă bomba este activă
 
-    // Bomb's position on the y-axis.
-    int m_y;
+public:
+    Bomb(int x, int y);
+    ~Bomb();
+
+    void Activate();
+    void Deactivate();
+    bool IsActive() const;
+    std::pair<int, int> GetPosition() const;
 };
--- ProiectMC/ServerMC/src/Bomb.cpp
+++ ProiectMC/ServerMC/src/Bomb.cpp
@@ -1,42 +1,22 @@
 ﻿#include "../include/Bomb.h"
-#include <iostream>
-#include <vector>
+#include<iostream>
 
-Bomb::Bomb(int x, int y) : m_x(x), m_y(y) {}
+Bomb::Bomb(int x, int y) : posX(x), posY(y), isActive(false) {}
 
-// Detonates the bomb using a callback to destroy walls
-void Bomb::Detonate(std::function<void(int, int)> destroyWallCallback) {
-    for (int i = -1; i <= 1; ++i) {
-        for (int j = -1; j <= 1; ++j) {
-            if (i != 0 || j != 0) {
-                destroyWallCallback(m_x + i, m_y + j); // Apelăm callback-ul
-            }
-        }
-    }
+Bomb::~Bomb() {}
+
+void Bomb::Activate() {
+    isActive = true;
 }
-// Calculates the effects of the explosion on walls and players.
-void Bomb::CalculateExplosionEffects(
-    std::function<bool(int, int)> isWallDestructibleCallback,
-    std::function<void(int, int)> destroyWallCallback,
-    std::function<void(int, int)> applyDamageToPlayerCallback
-) {
-    for (int i = -1; i <= 1; ++i) {
-        for (int j = -1; j <= 1; ++j) {
-            int newX = m_x + i;
-            int newY = m_y + j;
 
-            // Check and destroy walls
-            if (isWallDestructibleCallback(newX, newY)) {
-                destroyWallCallback(newX, newY);
-                std::cout << "Explosion destroys wall at (" << newX << ", " << newY << ")\n";
-            }
-
-            // Apply damage to players
-            applyDamageToPlayerCallback(newX, newY);
-        }
-    }
+void Bomb::Deactivate() {
+    isActive = false;
 }
-bool Bomb::IsInactive() {
-    // Exemplu logică: schimbă cu logica ta reală
-    return false;
+
+bool Bomb::IsActive() const {
+    return isActive;
+}
+
+std::pair<int, int> Bomb::GetPosition() const {
+    return { posX, posY };
 }
